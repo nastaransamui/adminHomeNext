595ebcd526cb6a488450b57d94fddf2bb14bfacf
Majid <nastaran.samui@gmail.com>
2022-02-17 13:53:01 +0700

    vercel Translate fix

--- home/next-i18next.config.js
+++ home/next-i18next.config.js
@@ -1,4 +1,7 @@
 const { initReactI18next } = require('react-i18next');
+// Tell Next.js these files will be read at runtime by the below code:
+const path = require('path');
+path.resolve('./public/locales/');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
