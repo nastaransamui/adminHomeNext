e825198fd8ae12f00d06e9768931120b33a824f5
Majid <nastaran.samui@gmail.com>
2022-04-12 15:41:43 +0700

    update isVercel hazelCast not use

--- admin/helpers/hazelCast.js
+++ admin/helpers/hazelCast.js
@@ -1,39 +1,41 @@
 var Client = require('hazelcast-client').Client;
 
 function createClientConfig() {
-  const isVercel = process.env.NEXT_PUBLIC_SERVERLESS == 'true' ? true : false;
-
   const lifecycleListener = (state) => {
     console.log('Lifecycle Event >>> ' + state);
   };
-  console.log(`isVercel: ${isVercel}`);
-  if (!isVercel) {
-    return {
-      clusterName: 'dev',
-      network: {
-        connectionTimeout: 6000,
-        clusterMembers: ['127.0.0.1:5701'],
+
+  return {
+    clusterName: 'dev',
+    network: {
+      connectionTimeout: 6000,
+      clusterMembers: ['127.0.0.1:5701'],
+    },
+    lifecycleListeners: [lifecycleListener],
+    connectionStrategy: {
+      asyncStart: false,
+      reconnectMode: 'ASYNC',
+      connectionRetry: {
+        initialBackoffMillis: 100000,
+        maxBackoffMillis: 60000,
+        multiplier: 2,
+        clusterConnectTimeoutMillis: 1,
+        jitter: 0.2,
       },
-      lifecycleListeners: [lifecycleListener],
-      connectionStrategy: {
-        asyncStart: false,
-        reconnectMode: 'ASYNC',
-        connectionRetry: {
-          initialBackoffMillis: 100000,
-          maxBackoffMillis: 60000,
-          multiplier: 2,
-          clusterConnectTimeoutMillis: 1,
-          jitter: 0.2,
-        },
-      },
-    };
-  }
+    },
+  };
 }
 
 const hazelCast = async () => {
+  const isVercel = process.env.NEXT_PUBLIC_SERVERLESS == 'true' ? true : false;
+
   try {
-    const hz = await Client.newHazelcastClient(createClientConfig());
-    return { hzErrorConnection: false, hz: hz };
+    if (!isVercel) {
+      const hz = await Client.newHazelcastClient(createClientConfig());
+      return { hzErrorConnection: false, hz: hz };
+    } else {
+      return { hzErrorConnection: true, hz: null };
+    }
   } catch (error) {
     return { hzErrorConnection: true, hz: null };
   }
